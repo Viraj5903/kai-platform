aea5f9a36b6108333b61d4201e85ebdfc830235f
Kishore Murugan <kishoremuruganmk@gmail.com>
2024-07-14 08:15:49 -0400

    Aligned the text center

--- frontend/templates/Chat/QuickActions/styles.js
+++ frontend/templates/Chat/QuickActions/styles.js
@@ -25,35 +25,37 @@ const styles = {
       position: 'relative',
       padding: '5px 10px',
       paddingLeft: '40px',
+      display: 'flex',
       flexDirection: 'row',
       flex: '1',
       backgroundColor: 'transparent',
-      color: 'rgb(88,20,244)',
-      border: '2px solid rgb(88,20,244)',
+      color: '#5e20f4',
+      border: '2px solid #5e20f4',
       borderRadius: '10px',
       cursor: 'pointer',
+      alignItems: 'center',
       transition: 'background-color 0.3s ease, color 0.3s ease',
       '&:hover': {
-        backgroundColor: 'rgb(88,20,244)',
+        backgroundColor: '#5e20f4',
         color: 'white',
-        '&::before': { // star transition
-          background: 'white', 
+        '&::before': {
+          background: 'white',
           transition: 'background-color 0.3s ease, color 0.3s ease',
         },
       },
-      '&::before': { // star shape styling
+      '&::before': {
         content: '""',
         position: 'absolute',
         width: '23px',
         height: '23px',
         left: '8px',
-        top: '17px',
-        background: 'rgb(88,20,244)',
-        clipPath: 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)', // CSS star shape
-        
+        top: '50%',
+        transform: 'translateY(-50%)',
+        background: '#5e20f4',
+        clipPath: 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)', 
       },
     },
-  },
+  },  
   /**
    * Styles for the text of a quick action
    */
